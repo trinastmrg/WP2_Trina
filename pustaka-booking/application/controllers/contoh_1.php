450f5779d17fcf7f331b1d1a773bd25fa8f8b634
trinastmrg <situmorangtrinaversi@gmail.com>
2022-04-19 15:11:44 +0700

    first commit

---
+++ pustaka-booking/application/controllers/contoh_1.php
@@ -0,0 +1,13 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Trina versi situmorang <br>";
+		echo "Nim : 17190179<br>";
+		echo "Fakultas : Teknik dan Informasi<br>";
+		echo "Jurusan : Teknologi Informasi<br>";
+		echo "Jenis Kelamin : Perempuan ";
+	}
+}
